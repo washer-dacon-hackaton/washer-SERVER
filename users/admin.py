c274460deb80b3bad90b46fb54f1c9b9bb7483fe
(yeseul) <(choiyeseul@g.hongik.ac.kr)>
2024-07-30 23:26:18 +0900

    [feat] User 모델 migration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+'''
+    class CustomUserAdmin(UserAdmin):
+        # 유저 리스트에 보여질 필드 지정
+        list_display = ('user_id','name', 'nationality','nickname','email','created_at') 
+'''
+
+admin.site.register(User)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    created_at = models.DateTimeField(verbose_name='가입시간', auto_now_add=True)
+    name = models.CharField(verbose_name='이름',max_length=255)
+    nationality = models.CharField(verbose_name='국적',max_length=255)
+    email = models.EmailField(verbose_name='이메일',unique=True)
+    nickname = models.CharField(verbose_name='닉네임',max_length=255, unique=True)
+
